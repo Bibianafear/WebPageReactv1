2bde25dc402360d6c2ee82bd7d837ce97dc640e5
Bibianakdjh <bibiana.velasan@hotmail.com>
2025-04-01 14:46:37 -0600

    Agregado archivo _redirects para Netlify

--- src/componentes/home/slider/Slider.js
+++ src/componentes/home/slider/Slider.js
@@ -15,21 +15,21 @@ class Slider extends React.Component {
 		    </ol>
 		    <div className="carousel-inner">
 		        <div className="carousel-item active">
-		            <img className="img-fluid" src="https://blog.nubecolectiva.com/wp-content/uploads/2019/10/img_destacada_blog_devs-5-930x360.png" />
+		            <img className="img-fluid" alt="logo" src="https://blog.nubecolectiva.com/wp-content/uploads/2019/10/img_destacada_blog_devs-5-930x360.png" />
 		            <div className="elementos">
-		                <a className="btn btn-lg btn-primary" href="https://blog.nubecolectiva.com/proteccion-de-rutas-o-vistas-mediante-contrasena-en-laravel-6-2/" target="_blank" role="button">Leer más</a>
+		                <a className="btn btn-lg btn-primary" href="https://blog.nubecolectiva.com/proteccion-de-rutas-o-vistas-mediante-contrasena-en-laravel-6-2/" target="_blank" rel="noreferrer noopener" role="button">Leer más</a>
 		            </div>
 		        </div>
 		        <div className="carousel-item">
-		            <img className="img-fluid" src="https://blog.nubecolectiva.com/wp-content/uploads/2019/08/edit_img_destacada_blog_devs-930x360.png" />
+		            <img className="img-fluid" alt="logo" src="https://blog.nubecolectiva.com/wp-content/uploads/2019/08/edit_img_destacada_blog_devs-930x360.png" />
 		            <div className="elementos">
-		                <a className="btn btn-lg btn-primary" href="https://blog.nubecolectiva.com/5-tips-para-mejorar-la-ux-experiencia-del-usuario-de-un-menu-web-creado-con-bootstrap-4" target="_blank" role="button">Leer más</a>
+		                <a className="btn btn-lg btn-primary" href="https://blog.nubecolectiva.com/5-tips-para-mejorar-la-ux-experiencia-del-usuario-de-un-menu-web-creado-con-bootstrap-4" target="_blank" rel="noreferrer noopener" role="button">Leer más</a>
 		            </div>
 		        </div>
 		        <div className="carousel-item">
-		            <img className="img-fluid" src="https://blog.nubecolectiva.com/wp-content/uploads/2019/10/edit_img_destacada_blog_devs-3-930x360.png" />
+		            <img className="img-fluid" alt="logo" src="https://blog.nubecolectiva.com/wp-content/uploads/2019/10/edit_img_destacada_blog_devs-3-930x360.png" />
 		            <div className="elementos">
-		                <a className="btn btn-lg btn-primary" href="https://blog.nubecolectiva.com/como-crear-animaciones-en-android-trasladar-elemento-en-eje-x-y-java/" target="_blank" role="button">Leer más</a>
+		                <a className="btn btn-lg btn-primary" href="https://blog.nubecolectiva.com/como-crear-animaciones-en-android-trasladar-elemento-en-eje-x-y-java/" target="_blank" rel="noreferrer noopener" role="button">Leer más</a>
 		            </div>
 		        </div>
 		    </div>
